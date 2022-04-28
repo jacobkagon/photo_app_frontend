02854fb9da953a5038ce7f89679ef5fa13f5f3ef
JakesProjects1 <jacob@kagon.net>
2022-04-28 15:50:32 -0400

    reconfig api url

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import "./App.css";
 import UploadForm from "./components/form/UploadForm";
 import Home from "./components/content/Home";
 
+import React, { useState, useEffect } from "react";
+
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  function updatePosts(post) {
+    setPosts([...posts, post]);
+  }
+
+  useEffect(() => {
+    fetch("https://don-photo-app-backend.herokuapp.com/api/v1/photos")
+      .then((resp) => resp.json())
+      //.then((data) => console.log(data))
+      .then((data) => setPosts(data));
+  }, []);
+
   return (
     <div>
       <h1>Don Ashbrook</h1>
-      <UploadForm />
-      <Home />
+      <UploadForm updatePosts={updatePosts} />
+      <Home posts={posts} />
       <div></div>
     </div>
   );
--- src/components/content/Home.js
+++ src/components/content/Home.js
@@ -1,18 +1,11 @@
 import React, { useState, useEffect } from "react";
 import Card from "./blog/Card";
-function Home() {
-  const [post, setPost] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:3000/api/v1/photos")
-      .then((resp) => resp.json())
-      //.then((data) => console.log(data))
-      .then((data) => setPost(data));
-  }, []);
+function Home(props) {
+  const { posts } = props;
 
   return (
     <div>
-      <Card post={post} />
+      <Card post={posts} />
     </div>
   );
 }
