df489130f9d7a4056832a1ff365b8fa2b4c7db48
Mauro Dorigo <mauro.dorigo@warschawski.com>
2022-05-22 16:02:04 -0400

    home, card css

--- src/components/content/Home.js
+++ src/components/content/Home.js
@@ -4,17 +4,21 @@ import NavBar from 'components/content/NavBar';
 import ReactDOM from 'react-dom';
 import { useMatch } from '@tanstack/react-location';
 
+import '../../styles/home.scss';
+
 function Home() {
   const {
     data: { posts },
   } = useMatch();
 
   return (
-    <div>
+    <div className="main">
       <NavBar />
+      <div className="images_container">
       {posts.data.map((image) => (
         <Card image={image} />
       ))}
+      </div>
     </div>
   );
 }
--- src/components/content/blog/Card.js
+++ src/components/content/blog/Card.js
@@ -1,10 +1,12 @@
 import { Image } from 'cloudinary-react';
-import 'card.css';
+import '../../../styles/card.scss';
 function Card(props) {
   const { image } = props;
-  return (
-    <div>
-      <img key='{image}' src={image} />
+  return ( 
+    <div className='wrapper'>
+      <a href='/' class='thumbnail-wrapper'>
+        <img key='{image}' src={image} className='thumbnail'/>
+      </a>
     </div>
   );
 }
